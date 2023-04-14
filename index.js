764b50d02dcac76c2635bd23e4037e9ac55ff9b6
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-04-14 14:04:38 +0100

    Fixed a bug on the callback block

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+// console.log("Before");
+// const user = getUser(1);
+// console.log("After");
+// console.log("The third");
+
+// function getUser(id) {
+//     setTimeout(() => {
+//         console.log("Getting a user from the database", id);
+//         return {id: id, gitHubUserName: "ElozinoLopez"}    // returning an object. It will not be called in the program unless you use a method to deal with the asynchronous code
+        
+//     }, 3000)
+// }
+
+// To get the return object we have to use any of the methods for dealing with asynchronous codes.
+// These methods are: Callbacks, Promises, and Async and Await
+
+// CALLBACKS
 console.log("Before");
-const user = getUser(1);
+const user = getUser(1, function(user){
+    console.log(user);
+});
 console.log("After");
 console.log("The third");
 
-function getUser(id) {
+function getUser(id, callback) {
     setTimeout(() => {
-        console.log("Getting a user from the database", id);
-        return {id: id, gitHubUserName: "ElozinoLopez"}    // returning an object. It will not be called in the program unless you use a method to deal with the asynchronous code
+        console.log("Getting a user from the database");
+        callback ({id: id, gitHubUserName: "ElozinoLopez"})    // replace return with callback
         
     }, 3000)
 }
