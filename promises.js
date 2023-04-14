aa3576c84e580d3511b7c2721c84f0244c7d058d
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-04-14 23:50:05 +0100

    Added the .catch property to catch errors

--- promises.js
+++ promises.js
@@ -1,9 +1,12 @@
 // Creating the promise object
-const p = new Promise(function(resolve, reject) {
-    resolve (1);
-
-})
-console.log(p);
 // const p = new Promise((resolve, reject) => {
 
 // })
+const p = new Promise(function(resolve, reject) {
+    // resolve (1);
+    //for reject, do not pass a string... rather, pass an error object
+    reject(new Error("Fail!"));
+});
+
+p.then(result => console.log(result));  // p.then gives us access to the successful operation and we can log it
+p.catch(err => console.log('Error', err.message));
