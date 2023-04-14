d4b172cd599fb8a955872dc9c837a26997615cf3
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-04-15 00:29:14 +0100

    Promise with resolve and reject in use

--- promises.js
+++ promises.js
@@ -10,3 +10,17 @@ const p = new Promise(function(resolve, reject) {
 
 // p.then(result => console.log(result));  // p.then gives us access to the successful operation and we can log it
 p.catch(err => console.log('Error', err.message));
+
+// Always pass in your resolve and reject
+const promise = new Promise(function(resolve, reject) {
+    let x = 4
+    if(x < 5) {
+        resolve('Success');
+    } else {
+        reject(new Error('Error Message'));
+    }
+});
+
+promise
+    .then(result => console.log(result))
+    .catch(error => console.log('Error', error.message));
