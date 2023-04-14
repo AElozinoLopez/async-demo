ed69335e3f472ab1f9b67f2d2dfb520b53b86d47
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-04-14 13:42:57 +0100

    REturned an object in the function

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 console.log("Before");
-getUser(1);
+const user = getUser(1);
 console.log("After");
 console.log("The third");
 
 function getUser(id) {
     setTimeout(() => {
         console.log("Getting a user from the database", id);
+        return {id: id, gitHubUserName: "ElozinoLopez"}
     }, 3000)
 }
