fba2c570d1a49ffa977c00d8564810dd36b975a8
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-04-15 23:50:25 +0100

    Used the .all method to consume the promise

--- promise-api.js
+++ promise-api.js
@@ -15,6 +15,7 @@ rejectedPromise.catch(err => console.log(err.message))
 // RUNNING PARALLEL PROMISES
 // Let's say you want to get 2 APIs - Facebook API and Twitter API and use the result for an operation
 
+// Calling the first api
 const firstApi = new Promise((resolve) => {
     setTimeout(() => {
         console.log('Calling the first API...')
@@ -22,10 +23,15 @@ const firstApi = new Promise((resolve) => {
     })
 })
 
+// Calling the second api
 const secondApi = new Promise((resolve) => {
     setTimeout(() => {
         console.log('Calling the second API')
         resolve(2)
     })
 })
+
+// Calling both promises. This makes use of the .all() method of promise and it returns an array of objects
+Promise.all([firstApi, secondApi])
+    then(result => console.log(result))
 
