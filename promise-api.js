1b56ac4f8cc63aeae45da7c01e70b115a7a2dc7c
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-04-15 23:31:10 +0100

    Created a promise object to call the first api

--- promise-api.js
+++ promise-api.js
@@ -1,5 +1,6 @@
 // Creating Settled Promises
 
+// Creating a resolved promise
 const resolvedPromise = Promise.resolve (1);
 
 resolvedPromise.then(result => console.log(result))
@@ -9,3 +10,15 @@ resolvedPromise.then(result => console.log(result))
 const rejectedPromise = Promise.reject(new Error('Enter reject reason'))
 
 rejectedPromise.catch(err => console.log(err.message))
+
+
+// RUNNING PARALLEL PROMISES
+// Let's say you want to get 2 APIs - Facebook API and Twitter API and use the result for an operation
+
+const firstApi = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Calling the first API...')
+        resolve(1)
+    })
+})
+
