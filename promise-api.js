043ab8913aa57a067e5eafa45a0e4eb500a31305
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-04-15 22:48:18 +0100

    Created a rejected promise

--- promise-api.js
+++ promise-api.js
@@ -6,3 +6,6 @@ resolvedPromise.then(result => console.log(result))
 
 // Creating a Rejected Promise
 
+const rejectedPromise = Promise.reject(new Error('Enter reject reason'))
+
+rejectedPromise.catch(err => console.log(err.message))
