fc919b86e47a664a512c3ed316dd9ad621e63082
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-04-16 02:35:52 +0100

    Rewrote the axios function using await

--- promises.js
+++ promises.js
@@ -174,3 +174,9 @@ function getUserCommits (repos) {
 }
 
 
+// Applying the async and await to the axios function
+
+const URL = `https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`;
+const result = await axios.get(URL)
+console.log(result.data);
+    
